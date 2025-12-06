4d14bb28edfaccbfcfdb6672d0c861879aa046f4
hemantch01 <hemantchaudhary905@gmail.com>
2025-12-06 18:30:32 +0000

    added #3 common type package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const userSchema = z.object({
+    name: z.string().min(3).max(15),
+    email: z.string(),
+    password:z.string()
+});
+
+export const signinSchema = z.object({
+    username: z.string().min(3).max(15),
+    password: z.string()
+})
+
+export const createRoomSchema = z.object({
+    name: z.string().min(3).max(15);
+})
