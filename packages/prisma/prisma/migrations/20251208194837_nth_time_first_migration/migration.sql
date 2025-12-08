c197ab187a153b64782eae63837fd91973244a3a
hemantch01 <hemantchaudhary905@gmail.com>
2025-12-08 19:58:26 +0000

    feat:fixed the prisma issue prolly have to check implementation 7.1.0

--- packages/prisma/prisma/migrations/20251208194837_nth_time_first_migration/migration.sql
+++ packages/prisma/prisma/migrations/20251208194837_nth_time_first_migration/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "photo" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -14,7 +14,7 @@ CREATE TABLE "Room" (
     "id" SERIAL NOT NULL,
     "slug" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "adminId" INTEGER NOT NULL,
+    "adminId" TEXT NOT NULL,
 
     CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
 );
@@ -24,7 +24,7 @@ CREATE TABLE "Chat" (
     "id" SERIAL NOT NULL,
     "roomId" INTEGER NOT NULL,
     "message" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
 );
