951caa20cbc2036a0a3532a6a8559514e1853f04
hemantch01 <hemantchaudhary905@gmail.com>
2025-12-09 17:46:32 +0000

    added signin route

--- apps/backend-http/src/routes/routes.ts
+++ apps/backend-http/src/routes/routes.ts
@@ -2,6 +2,8 @@ import express,{Router,Request,Response} from "express";
 //import { verifiedUser } from "../middleWares/verifiedUserCheck";
  import {prisma} from "@repo/prisma/types";
 import { userSchema } from "@repo/types";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/common-backend/config";
 export const routeHandler: Router = express.Router();
 
 routeHandler.post("/signup",signupHandler);
@@ -49,10 +51,41 @@ async function signupHandler(req:Request,res:Response){
 
 
 
-function signinHandler(req:Request,res:Response){
-res.json({
-    msg: "this is signin route"
-})
+async function signinHandler(req:Request,res:Response){
+    const {name ,email,password} = req.body;
+    const parsedUserData = userSchema.safeParse({
+        name,email,password
+    });
+    if(!parsedUserData.success){
+        res.json({
+            msg: "problem with parsing, probably req.body has not correct inputs"
+        })
+    }
+    try {
+        const user = await prisma.user.findFirst({
+            where:{
+                email: req.body.email,
+                password:req.body.password
+            }
+        });
+        if(user===null){
+            return res.status(401).json({
+                msg:"you are not signup user please signup"
+            })
+        }
+        const token = jwt.sign({
+            email:req.body.email,
+            name:req.body.name
+        },JWT_SECRET);
+        res.json({
+            msg: "you have sucessfully logged in",
+            token
+        })
+    } catch(e){
+        res.json({
+            msg:"there is problem in interacting with db"
+        })
+    }
 }
 
 
