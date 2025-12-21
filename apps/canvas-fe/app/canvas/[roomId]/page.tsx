7e3a00517ef84060c47ebee378b84c4f17e36248
hemantch01 <hemantchaudhary905@gmail.com>
2025-12-21 19:40:10 +0000

    added first shape---> rectangle

--- apps/canvas-fe/app/canvas/[roomId]/page.tsx
+++ apps/canvas-fe/app/canvas/[roomId]/page.tsx
@@ -11,15 +11,41 @@ export default function Room() {
       h: window.innerHeight,
     });
   }, []);
-  useEffect(() => {
 
-    const canvas = cRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+  useEffect(()=>{
+        const canvas = cRef.current;
+        if(!canvas)return ;
+        const ctx = canvas.getContext("2d");
+        if(!ctx)return ;
+        
 
-    ctx.strokeRect(200, 200, 100, 100);
-  }, [size.h,size.w]);
+        let startX = 0;
+        let startY =  0;
+        let width = 0;
+        let height = 0;
+        let isStarted = false;
+        canvas.addEventListener("mousedown",(e)=>{
+            isStarted =true;
+            startX = e.clientX;
+            startY = e.clientY;
+        })
+        canvas.addEventListener("mousemove",(e)=>{
+            if(isStarted){
+                width = e.clientX-startX;
+                height = e.clientY-startY;
+                ctx.clearRect(0,0,canvas.width,canvas.height);
+                ctx.strokeRect(startX,startY,width,height);
+            }
+        })
+        canvas.addEventListener("mouseup",(e)=>{
+            isStarted  = false;
+            width = e.clientX-startX;
+            height = e.clientY-startY;
+            // ctx.clearRect(0,0,canvas.width,canvas.height)
+            ctx.strokeRect(startX,startY,width,height);
+        })
+        
+  },[size.h,size.w])
 
   return (
     <canvas
